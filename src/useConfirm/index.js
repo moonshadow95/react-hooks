a9a16ad0a48b7ebb20106f47c0a9435d87c65b0a
ictus <ictus@ictusui-MacBookPro.local>
2021-12-17 09:49:20 +0900

    useConfirm

---
+++ src/useConfirm/index.js
@@ -0,0 +1,14 @@
+import {useConfirm} from "./useConfirm";
+
+const App = () => {
+    const deleteWorld = () => console.log("Deleted the world")
+    const abort = () => console.log("Aborted")
+    const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort)
+    return (
+        <div className="App">
+            <button onClick={confirmDelete}>Delete the world</button>
+        </div>
+    );
+};
+
+export default App;
